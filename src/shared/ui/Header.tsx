880d753398905de399bd5d40035c9e0799023540
Mykyta Sloboda <m.havryliuk@sloboda-studio.com>
2024-09-09 00:59:33 +0300

    icons & elements

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,7 +1,7 @@
 import { DarkModeToggle } from "@shared/ui/DarkModeToggle";
 
 export const Header = () => (
-    <header className="border-b border-line dark:border-lineDark dark:bg-darkBg py-3 flex">
+    <header className="border-b border-line bg-white dark:border-lineDark dark:bg-darkBg py-3 flex">
         <div className="container">
             < DarkModeToggle />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        darkBg: '#111928',
-        text: '#667085',
-        darkText: '#101828',
-        lightBg: '#fcfcfd',
-        line: '#eaecf0',
-        lineDark: '#374151'
-      }
+        darkBg: "#111928",
+        text: "#667085",
+        darkText: "#101828",
+        lightBg: "#fcfcfd",
+        line: "#eaecf0",
+        lineDark: "#374151",
+        orangeBg: "#fffcf5",
+        lightDark: "#17202e",
+      },
     },
   },
   plugins: [],
-}
+};
