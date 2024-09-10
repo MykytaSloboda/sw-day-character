e4325b3c0d71825361e25e074604c47fc5dab526
Mykyta Sloboda <m.havryliuk@sloboda-studio.com>
2024-09-10 12:49:03 +0300

    added data to profile section

--- src/pages/CharacterOfTheDay.tsx
+++ src/pages/CharacterOfTheDay.tsx
@@ -1,15 +1,30 @@
 import { BaseLayout, ProfileSection } from "@shared/ui";
 import { DataContext } from "@app/context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { getFetchedData } from "@app/services";
+import { IPlanet } from "@shared/types";
 
 export const CharacterOfTheDay = () => {
   const data = useContext(DataContext);
+  const [homeName, setHomeName] = useState("");
 
-  console.log(data);
+  const getHomeName = async () => {
+    const homeworldData = await getFetchedData<IPlanet>(
+      data?.character.homeworld || ""
+    );
+
+    setHomeName(homeworldData.name);
+  };
+
+  if (data) {
+    getHomeName();
+  }
 
   return (
     <BaseLayout>
-      {data?.character && <ProfileSection character={data?.character} />}
+      {data?.character && (
+        <ProfileSection character={data?.character} homeName={homeName} />
+      )}
     </BaseLayout>
   );
 };
